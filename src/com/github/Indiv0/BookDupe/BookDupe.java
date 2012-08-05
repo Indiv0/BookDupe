491f94f8731f25ce122a897d1557183e71e18816
Indiv0 <indiv0soft@gmail.com>
2012-08-05 17:35:46 -0400

    Fixed formatting to fit bukkit guidelines

--- src/com/github/Indiv0/BookDupe/BookDupe.java
+++ src/com/github/Indiv0/BookDupe/BookDupe.java
@@ -7,31 +7,27 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BookDupe extends JavaPlugin {
-	
-	// Initializes an ItemCraftListener.
-	public final ItemCraftListener blockListener = new ItemCraftListener();
-	
-	public void onEnable(){
-		// Retrieves an instance of the PluginManager.
-		PluginManager pm = getServer().getPluginManager();
-		
-		// Registers the blockListener with the PluginManager.
-		pm.registerEvents(this.blockListener, this);
-		
-		// Creates a new recipe to serve as the basis for the book duplication feature.
-		ItemStack craftable = new ItemStack(Material.FEATHER);
-		ShapelessRecipe recipe = new ShapelessRecipe(craftable);
-		recipe.addIngredient(Material.WRITTEN_BOOK);
-		recipe.addIngredient(Material.BOOK_AND_QUILL);
-		
-		// Adds the recipe to the server.
-		getServer().addRecipe(recipe);
-		
-		// Prints a message to the server confirming successful initialization of the plugin.
-		getLogger().info("BookDupe has initialized successfully.");
-	}
-	 
-	public void onDisable(){ 
-	 
-	}
+
+    // Initializes an ItemCraftListener.
+    public final ItemCraftListener blockListener = new ItemCraftListener();
+
+    public void onEnable(){
+        // Retrieves an instance of the PluginManager.
+        PluginManager pm = getServer().getPluginManager();
+
+        // Registers the blockListener with the PluginManager.
+        pm.registerEvents(this.blockListener, this);
+
+        // Creates a new recipe to serve as the basis for the book duplication feature.
+        ItemStack craftable = new ItemStack(Material.FEATHER);
+        ShapelessRecipe recipe = new ShapelessRecipe(craftable);
+        recipe.addIngredient(Material.WRITTEN_BOOK);
+        recipe.addIngredient(Material.BOOK_AND_QUILL);
+
+        // Adds the recipe to the server.
+        getServer().addRecipe(recipe);
+
+        // Prints a message to the server confirming successful initialization of the plugin.
+        getLogger().info("BookDupe has initialized successfully.");
+    }
 }
